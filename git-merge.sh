f1462853a9fa15e6704560562d3072e2f7eab938
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2019-08-06 10:19:35 +0200

    add merge script

---
+++ git-merge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# merges the current branch into the specified one and instantly pushes the changes
+
+CURRENT="$(git symbolic-ref --short HEAD)"
+TARGET="$1"
+
+if [ -z "$TARGET" ]; then
+  echo "target branch not specified"
+  exit 1
+fi
+
+if [ -z "$(git status --porcelain)" ]; then
+  echo "merging ${CURRENT} into ${TARGET}"
+  git checkout "$TARGET"
+  git pull
+  git merge "$CURRENT"
+  git push
+  git checkout "$CURRENT"
+else
+  echo "working directory not clean"
+  exit 1
+fi
