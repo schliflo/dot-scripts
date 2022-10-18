04951f6f5319ef2bb4550a455f7d9d6c79222703
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2022-10-18 19:18:06 +0200

    use nvm to manage node version and auto upgrade to latest

--- mac-upgrade.sh
+++ mac-upgrade.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+
 brew update
 brew upgrade
 brew cu -ac
 brew cleanup
+nvm install node
+nvm use node
 npm update -g
 composer global update
